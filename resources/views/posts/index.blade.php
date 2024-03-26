8d86e0acad05012ba714a53bd17b70a85f449433
subrata6630 <subrata6630@gmail.com>
2024-03-26 16:26:50 +0600

    add blog layout

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,16 @@
+<x-post-layout>  
+ <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+    <div class="p-6 text-gray-900">
+        All posts
+    </div>
+ </div>
+
+
+ <x-slot name="side">
+    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-6 text-gray-900 dark:text-gray-100">
+            Side
+        </div>
+    </div>
+</x-slot>
+</x-post-layout>      
