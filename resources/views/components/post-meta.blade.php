43f70cff3904c3bd202452ba4a9cdfe61cd8a4e0
subrata6630 <subrata6630@gmail.com>
2024-03-30 02:15:21 +0600

    add Showing Tags

--- resources/views/components/post-meta.blade.php
+++ resources/views/components/post-meta.blade.php
@@ -1,8 +1,18 @@
 <div>
     <strong>{{ $post->author }}</strong>/ {{ $post->date->diffForHumans() }}
-    <ul class="not-prose p-0 list-none flex items-center space-x-1 text-sm">
-        <li>
-            <a href="" class="text-blue-500 text-sm">Laravel</a>
-        </li>
-    </ul>
+
+    @if (count($post->tags))
+
+        <ul class="not-prose p-0 list-none flex items-center space-x-1 text-sm">
+
+                @foreach ($post->tags as $tag)
+
+                <li>
+                    <a href="" class="text-blue-500 text-sm">{{ $tag }}</a>
+                </li>
+
+                @endforeach
+        </ul>
+    @endif
+
 </div>
